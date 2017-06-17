30e3c36876b9e2793ab1183e267c07bbff2e5df8
1stmetro <andrew@apps-trader.com>
2017-06-17 08:32:26 +0100

    Update Constants.java
    Added constants for custom page markers

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
@@ -19,6 +19,10 @@ public class Constants {
 
     public static boolean DEBUG_MODE = false;
 
+    public static boolean SHOW_BORDER = false;
+    public static int BORDER_COLOR = 16777215;
+    public static int BORDER_WIDTH = 2;
+    
     /** Between 0 and 1, the thumbnails quality (default 0.3). Increasing this value may cause performance decrease */
     public static float THUMBNAIL_RATIO = 0.3f;
 
